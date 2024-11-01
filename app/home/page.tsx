e228273ecc6788a27db8bfa13f292032e2c3f5ca
yanasan <s.yanachi0817@gmail.com>
2024-11-01 16:12:02 +0900

    エラー修正

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,3 @@
-import Auth from "@/components/ui/Auth/Auth";
-
 export default function Home(){
     return (
         <div className="items-center justify-items-center min-h-screen min-w-screen p-8 pb-20 gap-20 sm:p-20">
