3159fcff3168eb3c536eac2b20b93e91dcdc13e1
yanasan <s.yanachi0817@gmail.com>
2024-11-01 16:18:34 +0900

    createUserPageを追加

---
+++ app/create_user/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateUser(){
+    return (
+        <div className="items-center justify-items-center min-h-screen min-w-screen p-8 pb-20 gap-20 sm:p-20">
+            CreateUser
+        </div>
+    );
+}
